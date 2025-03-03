81d6465f4a25bc8c6468093ae94674665397404a
Ibro Rebronja <27772860+IbroRebronja@users.noreply.github.com>
2025-03-03 15:42:30 +0100

    Updates

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 23815, hash: 'e77c878217f3e70fec3e122c11ddf236dd7fd16b82d971ef0e0552d943809e94', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17170, hash: '1a13353ae238c2ad09bc38d1b114d09180c835988423e06abec7fb2ee121d6a0', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 34010, hash: '1201696b154336fe571a3b8257239967069888e14cf1d3bc0d3e7f35f097d4a7', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 23815, hash: '63b79e71bab1dc97fbe9159a6c583de9969f771762c4b283372c3bf021af3bef', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17170, hash: 'd40f930c0dbb16c911cc990622cc77c906c3f0f8ef16ee58e036cf2478b8c269', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 34010, hash: '816313dc061ecc99baba17d96a8d4f92672cef4df2c2456b24c6a4d998cd9bba', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-YGK6NPXP.css': {size: 8034, hash: 'W+W+I9vX1Pg', text: () => import('./assets-chunks/styles-YGK6NPXP_css.mjs').then(m => m.default)}
   },
 };
